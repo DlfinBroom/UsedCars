fceed85988d649258534b51e8851fab129150578
lampert309 <lampert309@gmail.com>
2019-04-03 14:23:44 -0700

    Added an about page to the home controller

--- UsedCars/Controllers/HomeController.cs
+++ UsedCars/Controllers/HomeController.cs
@@ -6,23 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using UsedCars.Models;
 
-namespace UsedCars.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace UsedCars.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult About() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
